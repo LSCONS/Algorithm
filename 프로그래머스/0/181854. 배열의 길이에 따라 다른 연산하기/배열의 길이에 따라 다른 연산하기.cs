c6ab575025fa2b5004528c554625093e6267fc9e
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-07-19 00:47:08 +0900

    [level 0] Title: 배열의 길이에 따라 다른 연산하기, Time: 0.27 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181854. 배열의 길이에 따라 다른 연산하기/배열의 길이에 따라 다른 연산하기.cs
@@ -0,0 +1,17 @@
+using System;
+
+    public class Solution
+    {
+        public int[] solution(int[] arr, int n)
+        {
+            int length = arr.Length;
+            int startIndex = (length % 2) == 0 ? 1 : 0;
+
+            for(int i = startIndex; i < length; i += 2)
+            {
+                arr[i] += n;
+            }
+            
+            return arr;
+        }
+    }
