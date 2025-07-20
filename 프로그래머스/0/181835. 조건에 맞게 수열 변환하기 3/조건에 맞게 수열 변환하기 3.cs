4a41300a620a66342509fbc967e028360d7aab65
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-07-20 22:58:11 +0900

    [level 0] Title: 조건에 맞게 수열 변환하기 3, Time: 2.85 ms, Memory: 84.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/181835. 조건에 맞게 수열 변환하기 3/조건에 맞게 수열 변환하기 3.cs
@@ -0,0 +1,13 @@
+using System;
+
+    public class Solution
+    {
+        public int[] solution(int[] arr, int k)
+        {
+            for(int i = 0; i < arr.Length; i++)
+            {
+                arr[i] = (k % 2) == 0 ? arr[i] + k : arr[i] * k;
+            }
+            return arr;
+        }
+    }
