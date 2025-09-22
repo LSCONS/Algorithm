9827933e9e1cb082bb0398e43d2a64a4ccb6585e
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-09-22 23:18:21 +0900

    [level 0] Title: 주사위 게임 2, Time: 0.28 ms, Memory: 31.3 MB -BaekjoonHub

--- 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
+++ 프로그래머스/0/181930. 주사위 게임 2/주사위 게임 2.cs
@@ -1,25 +1,8 @@
 using System;
 
-public class Solution 
-{
-    public int solution(int a, int b, int c) 
-    {
+public class Solution {
+    public int solution(int a, int b, int c) {
         int answer = 0;
-        answer += a + b + c;
-        
-        if(a == b ||
-           b == c ||
-           c == a)
-        {
-            answer *= a * a + b * b + c * c;
-        }
-        
-        if(a == b && b == c)
-        {
-            answer *= a * a * a + b * b * b + c * c * c;
-        }
-        
-        
         return answer;
     }
 }
