1157b1156615228f76e37034c1f6ce126636232d
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-09-26 02:28:53 +0900

    [level 0] Title: 문자열 곱하기, Time: 0.38 ms, Memory: 31.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181940. 문자열 곱하기/문자열 곱하기.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Text;
+
+public class Solution 
+{
+    public string solution(string my_string, int k) 
+    {
+        StringBuilder answer = new StringBuilder();
+        
+        for(int i = 0; i < k; i++)
+        {
+            answer.Append(my_string);
+        }
+        
+        return answer.ToString();
+    }
+}
