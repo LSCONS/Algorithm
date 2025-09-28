ebeeaeb5dabb6e8e127125599e87bdfa1328a08b
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-09-28 14:46:58 +0900

    [level 0] Title: ad 제거하기, Time: 1.66 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181870. ad 제거하기/ad 제거하기.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+
+public class Solution 
+{
+    public string[] solution(string[] strArr) 
+    {
+        return strArr
+            .Where(x => !x.Contains("ad"))
+            .ToArray();
+    }
+}
