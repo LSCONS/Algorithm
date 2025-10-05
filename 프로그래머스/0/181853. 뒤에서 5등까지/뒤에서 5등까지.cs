e013a49d556e5cacaecf23d8d6fffb7dca32c6ea
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-10-06 03:38:16 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 3.64 ms, Memory: 31.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+public class Solution 
+{
+    public int[] solution(int[] num_list) 
+    {
+        return num_list
+            .OrderBy(x => x)
+            .Take(5)
+            .ToArray();     
+    }
+}
