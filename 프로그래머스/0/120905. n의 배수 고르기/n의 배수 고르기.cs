2c522de52e7f3a1df07db11df67f86fd0bd863e9
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-10-06 20:25:22 +0900

    [level 0] Title: n의 배수 고르기, Time: 1.44 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120905. n의 배수 고르기/n의 배수 고르기.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+
+public class Solution 
+{
+    public int[] solution(int n, int[] numlist) 
+    {
+        return numlist
+            .Where(x => x % n == 0)
+            .ToArray();
+    }
+}
