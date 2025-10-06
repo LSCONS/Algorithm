10cd61123e58089ba8644024ba861fb5a2630b2e
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-10-06 20:54:49 +0900

    [level 0] Title: 짝수는 싫어요, Time: 1.48 ms, Memory: 31.3 MB -BaekjoonHub

---
+++ 프로그래머스/0/120813. 짝수는 싫어요/짝수는 싫어요.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Linq;
+
+public class Solution 
+{
+    public int[] solution(int n) 
+    {
+        return Enumerable
+            .Range(1, n)
+            .Where(x => x % 2 == 1)
+            .ToArray();
+    }
+}
