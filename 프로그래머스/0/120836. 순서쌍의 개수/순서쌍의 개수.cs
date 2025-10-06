cc7565782b66f18c8390f40159c9ec973395dc3f
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-10-06 20:23:20 +0900

    [level 0] Title: 순서쌍의 개수, Time: 1.05 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120836. 순서쌍의 개수/순서쌍의 개수.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+
+public class Solution 
+{
+    public int solution(int n) 
+    {
+        HashSet<int> hash = new HashSet<int>();
+        for(int i = 1; i <= Math.Sqrt(n); i++)
+        {
+            if(n % i == 0)
+            {
+                hash.Add(i);
+                hash.Add(n / i);
+            }
+        }
+        return hash.Count;
+    }
+}
