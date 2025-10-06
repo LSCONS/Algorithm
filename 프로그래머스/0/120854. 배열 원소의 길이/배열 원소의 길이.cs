234cd643cbf06330fed96fff5aeff4fe2d3d8516
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-10-06 20:26:15 +0900

    [level 0] Title: 배열 원소의 길이, Time: 0.80 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120854. 배열 원소의 길이/배열 원소의 길이.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Linq;
+
+public class Solution 
+{
+    public int[] solution(string[] strlist) 
+    {
+        return strlist
+            .Select(x => x.Length)
+            .ToArray();
+    }
+}
