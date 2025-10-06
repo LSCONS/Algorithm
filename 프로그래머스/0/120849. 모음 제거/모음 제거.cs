5ea11e0d1ec107df8ff7d117c3edab215c82f232
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-10-06 20:40:39 +0900

    [level 0] Title: 모음 제거, Time: 3.22 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/120849. 모음 제거/모음 제거.cs
+++ 프로그래머스/0/120849. 모음 제거/모음 제거.cs
@@ -7,9 +7,6 @@ public class Solution
     public string solution(string my_string) 
     {
         HashSet<char> hash = new HashSet<char>(){'a', 'e', 'i', 'o', 'u'};
-        return new string(my_string
-            .ToCharArray()
-            .Where(x => !hash.Contains(x))
-            .ToArray());
+        return new string(my_string.ToCharArray().Where(x => !hash.Contains(x)).ToArray());
     }
 }
