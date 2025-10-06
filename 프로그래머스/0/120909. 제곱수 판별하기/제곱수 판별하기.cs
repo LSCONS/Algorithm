823d7d391bcbf9ee8944b6113aee3498527933da
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-10-06 20:36:13 +0900

    [level 0] Title: 제곱수 판별하기, Time: 0.35 ms, Memory: 31.7 MB -BaekjoonHub

--- 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cs
+++ 프로그래머스/0/120909. 제곱수 판별하기/제곱수 판별하기.cs
@@ -4,7 +4,6 @@ public class Solution
 {
     public int solution(int n) 
     {
-        int sqrt = (int)Math.Sqrt(n);
-        return sqrt * sqrt == n ? 1 : 2;
+        return Math.Sqrt(n) % 1 == 0 ? 1 : 2;
     }
 }
