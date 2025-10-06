ca2dc5913a3ba1b571c6049c5238d6cc2a9945a7
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-10-06 19:18:59 +0900

    [level 0] Title: 세균 증식, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120910. 세균 증식/세균 증식.cs
@@ -0,0 +1,13 @@
+using System;
+
+public class Solution 
+{
+    public int solution(int n, int t) 
+    {
+        for(int i = 0; i < t; i++)
+        {
+            n *= 2;
+        }
+        return n;
+    }
+}
