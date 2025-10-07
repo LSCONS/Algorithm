b7aed9224e4c9e76995cc5975f69e3f36c666783
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-10-07 19:31:27 +0900

    [level 0] Title: A로 B 만들기, Time: 5.77 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.cs
+++ 프로그래머스/0/120886. A로 B 만들기/A로 B 만들기.cs
@@ -4,7 +4,5 @@ using System.Linq;
 public class Solution 
 {
     public int solution(string before, string after) 
-    {
-        return new string(before.OrderBy(x => x).ToArray()) == new string(after.OrderBy(x => x).ToArray()) ? 1 : 0;
-    }
+        => new string(before.OrderBy(x => x).ToArray()) == new string(after.OrderBy(x => x).ToArray()) ? 1 : 0;
 }
