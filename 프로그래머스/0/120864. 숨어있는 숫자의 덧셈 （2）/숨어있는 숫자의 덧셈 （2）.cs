2066d2f06394a7740651869a07778874ba4bc67f
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-10-07 20:04:24 +0900

    [level 0] Title: 숨어있는 숫자의 덧셈 (2), Time: 3.31 ms, Memory: 31.6 MB -BaekjoonHub

--- 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cs
+++ 프로그래머스/0/120864. 숨어있는 숫자의 덧셈 （2）/숨어있는 숫자의 덧셈 （2）.cs
@@ -1,8 +1,3 @@
 using System;
 using System.Linq;
-
-public class Solution 
-{
-    public int solution(string my_string) 
-        => new string(my_string.Select(x => char.IsDigit(x) ? x : ' ').ToArray()).Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).Sum(); 
-}
+public class Solution {public int solution(string my_string) => new string(my_string.Select(x => char.IsDigit(x) ? x : ' ').ToArray()).Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(x => int.Parse(x)).Sum(); }
