47096c497df8bdc3581f70d7b50d859c3e50167e
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-10-07 19:21:29 +0900

    [level 0] Title: 모스부호 (1), Time: 2.87 ms, Memory: 30.8 MB -BaekjoonHub

---
+++ 프로그래머스/0/120838. 모스부호 （1）/모스부호 （1）.cs
@@ -0,0 +1,42 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Solution 
+{
+    public string solution(string letter) 
+    {
+        Dictionary<string, char> dict = new Dictionary<string, char>()
+        {
+            { ".-", 'a' },
+            { "-...", 'b' },
+            { "-.-.", 'c' },
+            { "-..", 'd' },
+            { ".", 'e' },
+            { "..-.", 'f' },
+            { "--.", 'g' },
+            { "....", 'h' },
+            { "..", 'i' },
+            { ".---", 'j' },
+            { "-.-", 'k' },
+            { ".-..", 'l' },
+            { "--", 'm' },
+            { "-.", 'n' },
+            { "---", 'o' },
+            { ".--.", 'p' },
+            { "--.-", 'q' },
+            { ".-.", 'r' },
+            { "...", 's' },
+            { "-", 't' },
+            { "..-", 'u' },
+            { "...-", 'v' },
+            { ".--", 'w' },
+            { "-..-", 'x' },
+            { "-.--", 'y' },
+            { "--..", 'z' },
+        };
+        
+        string[] split = letter.Split(" ");
+        return new string(split.Select(x => dict[x]).ToArray());
+    }
+}
