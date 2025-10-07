9f66c363377f0051ad2c347e153510492741bb46
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-10-07 18:46:44 +0900

    [level 0] Title: 중복된 문자 제거, Time: 3.23 ms, Memory: 31.2 MB -BaekjoonHub

--- 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.cs
+++ 프로그래머스/0/120888. 중복된 문자 제거/중복된 문자 제거.cs
@@ -4,5 +4,5 @@ using System.Linq;
 public class Solution 
 {
     public string solution(string my_string) 
-        => new string(my_string.GroupBy(x => x).Select(x => x.Key).ToArray());
+        => new string(my_string.Distinct().ToArray());
 }
