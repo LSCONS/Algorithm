882477e313d93480c58289630a92492936fd10a1
LSCONS <52284315+LSCONS@users.noreply.github.com>
2025-10-07 19:46:01 +0900

    [level 0] Title: k의 개수, Time: 26.80 ms, Memory: 35.2 MB -BaekjoonHub

--- 프로그래머스/0/120887. k의 개수/k의 개수.cs
+++ 프로그래머스/0/120887. k의 개수/k의 개수.cs
@@ -6,6 +6,7 @@ public class Solution
     public int solution(int i, int j, int k) 
         => Enumerable
         .Range(i, j - i + 1)
+        .Where(x => x.ToString().Contains(k.ToString()))
         .Select(x => x.ToString().Count(y => y == (char)(k + '0')))
         .Sum();
 }
